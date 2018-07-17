c3690298a7d6d7b8ebc7eba0c74a029f4175025d
QistChan <mail@chenyuqi.cn>
2018-07-17 16:44:16 +0800

    Init

---
+++ src/main/java/com/bitmoe/osp/qistchan/methane/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Bitmoe Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package com.bitmoe.osp.qistchan.methane;
+import org.apache.commons.logging.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        log.info("INFO");
+    }
+
+    public static Log log = LogFactory.getLog(Main.class);
+
+}
